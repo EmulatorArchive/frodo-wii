d66f8fde16d1866f1b6247e11ca8aebd7211e745
simon.kagstrom <simon.kagstrom@feb19cf6-d755-11dd-8179-53c65a4d6d95>
2009-01-06 13:35:57 +0000

    OK, fixed some minor issues

--- tags/build_release.sh
+++ tags/build_release.sh
@@ -8,10 +8,5 @@ fi
 
 DIR=`basename $1`
 
-find $DIR | xargs chmod a+r
-find $DIR -type d | xargs chmod a+x
-find $DIR -type f -and -name "shcov" | xargs chmod a+x
-find $DIR -type f -and -name "shlcov" | xargs chmod a+x
-
 # Exclude generated stuff and svn things
 tar --exclude="*~" --exclude="*.pyc" --exclude="*.pyo" --exclude=".svn*" -czf $DIR.tar.gz $DIR
